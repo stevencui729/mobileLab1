0696c5fc4c7ccf611f3fe6e9ad5a96852e87fdae
Steven Cui <stevencui729@gmail.com>
2019-01-31 17:48:54 -0600

    finished classification

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 import parse_data as pd
 import generateFeatures as gf
 import pprint as pp
+import os
 import sys
-
-def find_bounds(full_data):
-    activity_bounds = {}
-    for activity in full_data:
-        cur_traces = full_data[activity]
-        var_bounds = {'minX': sys.maxsize, 'maxX': -1, 'minY': sys.maxsize,
-            'maxY': -1, 'minZ': sys.maxsize, 'maxZ': -1}
-
-        for trace in cur_traces:
-            varDict = gf.generateFeatures(trace)[0]
-            for direction in varDict:
-                dir_max_key = 'max'+direction[0].upper()
-                dir_min_key = 'min'+direction[0].upper()
-                if varDict[direction] < var_bounds[dir_min_key]:
-                    var_bounds[dir_min_key] = varDict[direction]
-                if varDict[direction] > var_bounds[dir_max_key]:
-                    var_bounds[dir_max_key] = varDict[direction]
-
-        activity_bounds[activity] = var_bounds
-    return activity_bounds
 
 
 def classify_activity(trace):
     classified_activity = None
-    varDict, powerSpectrums, spectrumPeaks, relTime = gf.generateFeatures(trace)
+    varDict, powerSpectrums, midPowers, relTime = gf.generateFeatures(trace)
     varZ = varDict['zAccl']
-    if (varZ / 100000) > 0:
-        print("varZ less than 6 digits, identified as jumping")
+    midPowerZ = midPowers['zAccl']
+    if varZ > 300000:
+        print("varZ is: "+str(varZ)+" - greater than 300,000, identified as jumping")
         classified_activity = "Jumping"
     elif varZ < 5000:
-        print("varZ less 5000, identified as jumping")
+        print("varZ is: "+str(varZ)+" - less than 5,000, identified as standing")
         classified_activity = "Standing"
-    else: 
-
-
+    elif midPowerZ > 0.2:
+        print("midPowerZ is: "+str(midPowerZ)+" - greater than 0.2, identified as driving")
+        classified_activity = "Driving"
+    else:
+        print("midPowerZ is: "+str(midPowerZ)+" - less than 0.2, identified as walking")
+        classified_activity = "Walking"
+    return classified_activity
 
 
 def main():
-    parsed_data = pd.parse_data()
-    bounds = find_bounds(parsed_data)
-    pp.pprint(bounds)
-    # first_walking_trace = parsed_data['Walking'][0]
-    # first_driving_trace = parsed_data['Driving'][0]
-    # first_standing_trace = parsed_data['Standing'][0]
-    # first_jumping_trace = parsed_data['Jumping'][0]
-    # classify_activity(first_walking_trace)
+    # parsed_data = pd.parse_data_train()
+
+    # # playing arounds with bounds finder
+    # # bounds = find_bounds(parsed_data)
+    # # pp.pprint(bounds)
+
+    # # test running classifier on all the training data
+    # for activity in parsed_data:
+    #     print(activity)
+    #     cur_traces = parsed_data[activity]
+
+    #     for trace in cur_traces:
+    #         classify_activity(trace)
+
+
+    # running classifier on test data
+    parsed_data = pd.parse_data_test()
+    for trace in parsed_data:
+        print(trace)
+        cur_trace = parsed_data[trace]
+        classify_activity(cur_trace)
 
 if __name__ == "__main__":
     main()
--- parse_data.py
+++ parse_data.py
@@ -1,9 +1,8 @@
 import os
 import json
 
-work_dir = os.path.join(os.getcwd(), "activity-trainingdata")
 
-def parse_file(filepath):
+def parse_file_train(filepath):
     with open(filepath, 'r') as f:
         loaded_file_string = f.read().replace("'", '"')
         loaded_raw_data = json.loads(loaded_file_string)
@@ -29,16 +28,45 @@ def parse_file(filepath):
     return data_points
 
 
-def parse_data():
+def parse_data_train():
+    work_dir = os.path.join(os.getcwd(), "activity-trainingdata")
     parsed_data = {}
     for filename in os.listdir(work_dir):
         filepath = os.path.join(work_dir, filename)
         print("parsing file: "+filepath)
-        curData = parse_file(filepath)
+        curData = parse_file_train(filepath)
         parsed_data[curData[0]['activity']] = curData 
     
     return parsed_data
 
-if __name__ == "__main__":
-    data = parse_data()
-    print(len(data.keys()))
+def parse_file_test(filepath):
+    with open(filepath, 'r') as f:
+        loaded_file_string = f.read().replace("'", '"')
+        loaded_raw_data = json.loads(loaded_file_string)
+
+    try:
+        activity_name = loaded_raw_data['type']
+    except KeyError:
+        activity_name = None
+        print("could not find activity, setting `activity_name` to None")
+    
+    data_seq = loaded_raw_data['seq']
+    xList = [data_point['data']['xAccl'] for data_point in data_seq]
+    yList = [data_point['data']['yAccl'] for data_point in data_seq]
+    zList = [data_point['data']['zAccl'] for data_point in data_seq]
+    timeList = [data_point['time'] for data_point in data_seq]
+    parsed_dict = {"xAccl": xList, "yAccl": yList, "zAccl": zList, 
+        "time": timeList, "activity": activity_name}
+    
+    return parsed_dict
+
+def parse_data_test():
+    work_dir = os.path.join(os.getcwd(), "activity-test-dataset")
+    parsed_data = {}
+    for filename in os.listdir(work_dir):
+        filepath = os.path.join(work_dir, filename)
+        print("parsing file: "+filepath)
+        curData = parse_file_test(filepath)
+        parsed_data[filename] = curData 
+    
+    return parsed_data
